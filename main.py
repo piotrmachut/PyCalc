cf0f2f0745a0ca3c65c153ffdcc4ecc58185ba52
piotrmachut <piotr.machut@gmail.com>
2021-09-21 23:41:21 +0200

    Add next operations.

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
+def should_continue(result):
+    decission = input(f"Type 'y' to continue calculating with {result}, or type 'n' to exit.: ")
+    if decission == 'y':
+        return True
+    elif decission == 'n':
+        return False
+    else:
+        print("Wrong input!")
+
 operations = {
     "+": add,
     "-": subtract,
@@ -28,3 +37,14 @@ num2 = int(input("What's the second number?: "))
 result = operations[operation_symbol](num1, num2)
 
 print(f"{num1} {operation_symbol} {num2} = {result}")
+
+continue_calculation = should_continue(result)
+
+while continue_calculation:
+    operation_symbol = input("Pick next operation: ")
+    num1 = result
+    num2 = int(input("What's the next number?: "))
+    result = operations[operation_symbol](num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {result}")
+    continue_calculation = should_continue(result)
+
